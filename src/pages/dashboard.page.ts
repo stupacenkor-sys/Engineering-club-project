2f91b7231f07c857d4093406a282f0118f3dfe65
stupacenkor-sys <stupacenkor@gmail.com>
2026-09-22 13:39:31 +0300

    refactor(dashboard): remove unrelated locators and unused user config

--- src/pages/dashboard.page.ts
+++ src/pages/dashboard.page.ts
@@ -2,36 +2,27 @@ import { Page, Locator } from '@playwright/test';
 
 export class DashBoardPage {
   readonly page: Page;
-  readonly switchToEnglishlanguageButton: Locator;
-  readonly switchToUkrainianlanguageButton: Locator;
+
   readonly resumelearningButton: Locator;
   readonly continuelessonButton: Locator;
   readonly courseoutlineButton: Locator;
   readonly viewAllAssignmentsLink: Locator;
   readonly aiRecommendationStartRefresherButton: Locator;
-  readonly globalSearchInput: Locator;
-  readonly notificationsButton: Locator;
+
   constructor(page: Page) {
     this.page = page;
-    this.switchToEnglishlanguageButton = page.getByRole('button', { name: 'EN', description: 'English' });
-    this.switchToUkrainianlanguageButton = page.getByRole('button', { name: 'УК' });
+    
     this.resumelearningButton = page.getByRole('link', { name: 'Resume learning' });
     this.continuelessonButton = page.getByRole('link', { name: 'Continue lesson' });
     this.courseoutlineButton = page.getByRole('link', { name: 'Course outline' });
     this.viewAllAssignmentsLink = page.getByRole('link', { name: 'View all assignments' });
     this.aiRecommendationStartRefresherButton = page.getByRole('button', { name: 'Start refresher →' });
-    this.globalSearchInput = page.getByText('Search courses, lessons, docs…⌘K');
-    this.notificationsButton = page.getByRole('button', { name: 'Notifications, none unread' })
+   
 }
 async gotodashboard() {
     await this.page.goto('/dashboard');
   }
-async switchToEnglishlanguage() {
-    await this.switchToEnglishlanguageButton.click();
-  }
-async switchToUkrainianlanguage() {
-    await this.switchToUkrainianlanguageButton.click();
-  }
+
   async clickResumelearning() {
     await this.resumelearningButton.click();
   }
@@ -47,7 +38,5 @@ async switchToUkrainianlanguage() {
   async clickAiRecommendationStartRefresher() {
     await this.aiRecommendationStartRefresherButton.click();
   }
-  async clicknotificationsButton() {
-    await this.notificationsButton.click();
-  }
+
 }
