8e7b427950428af117c7b5ee3955d2aef2facbd2
Deker <dimon.bond.94@gmail.com>
2026-09-23 13:40:38 +0300

    refactor(skill-sheck): implements POM and use it in my tests

--- tests/ui/student/skill-check.spec.ts
+++ tests/ui/student/skill-check.spec.ts
@@ -1,33 +1,24 @@
 import { test, expect, Locator } from '@playwright/test';
+import { SkillCheckPage } from '@pages/skill-check.page';
+import { LoginPage } from '@pages/login.page';
 
-const STUDENT_EMAIL = 'maria@example.com';
-const STUDENT_PASSWORD = 'password123';
+// Change after the .env file is updated with the correct credentials
+const STUDENT_EMAIL = process.env.STUDENT_USERNAME!;
+const STUDENT_PASSWORD = process.env.PASSWORD!;
 
-test.describe('Passing skill checks by student', () => {
-  let emailInput: Locator;
-  let passwordInput: Locator;
-  let submitButton: Locator;
-
-  let areaFilter: Locator;
-  let levelFilter: Locator;
-
+test.describe.only('Passing skill checks by student', () => {
   test('should navigate to skill check page when clicking link', async ({
     page,
   }) => {
+    const loginPage = new LoginPage(page);
+
     await test.step('Go to login page', async () => {
-      await page.goto('/login');
+      await loginPage.goto();
       await expect(page).toHaveURL(/\/login/);
     });
 
     await test.step('Login as student', async () => {
-      emailInput = page.getByRole('textbox', { name: 'Email' });
-      passwordInput = page.getByRole('textbox', { name: 'Password' });
-      submitButton = page.getByRole('button', { name: 'Sign in' });
-
-      await emailInput.fill(STUDENT_EMAIL);
-      await passwordInput.fill(STUDENT_PASSWORD);
-      await submitButton.click();
-
+      await loginPage.login(STUDENT_EMAIL, STUDENT_PASSWORD);
       await expect(page).toHaveURL(/\/dashboard/);
     });
 
@@ -40,39 +31,28 @@ test.describe('Passing skill checks by student', () => {
   test('should check the filter when redirected to skill check page', async ({
     page,
   }) => {
+    const loginPage = new LoginPage(page);
+    const skillCheckPage = new SkillCheckPage(page);
+
     await test.step('Go to login page', async () => {
-      await page.goto('/login');
+      await loginPage.goto();
       await expect(page).toHaveURL(/\/login/);
     });
 
     await test.step('Login as student', async () => {
-      emailInput = page.getByRole('textbox', { name: 'Email' });
-      passwordInput = page.getByRole('textbox', { name: 'Password' });
-      submitButton = page.getByRole('button', { name: 'Sign in' });
-
-      await emailInput.fill(STUDENT_EMAIL);
-      await passwordInput.fill(STUDENT_PASSWORD);
-      await submitButton.click();
-
+      await loginPage.login(STUDENT_EMAIL, STUDENT_PASSWORD);
       await expect(page).toHaveURL(/\/dashboard/);
     });
 
     await test.step('Go to skill check page', async () => {
       await page.getByRole('link', { name: 'Skill Checks' }).click();
-      await expect(page).toHaveURL(/\/quizzes/);
+      await skillCheckPage.verifyURL();
     });
 
     await test.step('Verify filter is on the page', async () => {
-      areaFilter = page.getByText('Area', { exact: true });
-      levelFilter = page.getByText('Level', { exact: true });
-      const filterContainer = page
-        .locator('div.blueprint')
-        .filter({ has: areaFilter })
-        .filter({ has: levelFilter });
-
-      await expect(areaFilter).toBeVisible();
-      await expect(levelFilter).toBeVisible();
-      await expect(filterContainer).toBeVisible();
+      await expect(skillCheckPage.filterContainer).toBeVisible();
+      await expect(skillCheckPage.areaFilter).toBeVisible();
+      await expect(skillCheckPage.levelFilter).toBeVisible();
     });
   });
 });
